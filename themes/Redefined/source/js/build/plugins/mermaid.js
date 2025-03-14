5d66c35e70201f617434cbb6aecdcad9589fef0b
backyl <pbq34@qq.com>
2025-03-14 22:31:11 +0800

    first

---
+++ themes/Redefined/source/js/build/plugins/mermaid.js
@@ -0,0 +1,7 @@
+if (!0 === theme.plugins.mermaid.enable)
+  try {
+    swup.hooks.on("page:view", () => {
+      mermaid.initialize();
+    });
+  } catch (e) {}
+//# sourceMappingURL=mermaid.js.map
